b94665d15434627ffeb35e5027f6711bb7fc81c5
Lucas Gerisztein <lucas.gerisztein@crvsh.io>
2020-03-09 20:50:53 +0100

    Initial Commit

---
+++ RIck and Morty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RIck and Morty
+//
+//  Created by Lucas Gerisztein on 09.03.20.
+//  Copyright © 2020 Lucas Gerisztein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
